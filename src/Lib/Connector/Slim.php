2077996368d0568e85c8931c6c7f1b581c887028
herloct <herloct@gmail.com>
2016-10-07 16:51:18 +0700

    Remove `strict_types`.
    Because it throws warning on PHP < 7.0.

--- src/Lib/Connector/Slim.php
+++ src/Lib/Connector/Slim.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Herloct\Codeception\Lib\Connector;
 
 use Psr\Http\Message\UploadedFileInterface;
--- src/Module/Slim.php
+++ src/Module/Slim.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Herloct\Codeception\Module;
 
 use Codeception\Configuration;
