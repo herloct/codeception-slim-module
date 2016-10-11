5ef038cdde3e17a72aa71fca244c00e71c872746
herloct <herloct@gmail.com>
2016-10-12 04:37:34 +0700

    Because we also test the raw body.

--- tests/functional/SeeRawAndParsedBodyIsNotNullCept.php
+++ tests/functional/SeeRawAndParsedBodyIsNotNullCept.php
@@ -2,7 +2,7 @@
 $I = new FunctionalTester($scenario);
 
 $data = ['testing' => '123'];
-$I->wantTo('test that the parsed request body is returned in tact');
+$I->wantTo('test that the raw and parsed request body is returned in tact');
 $I->haveHttpHeader('content-type', 'application/json');
 $I->sendPOST('/rest', $data);
 $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
